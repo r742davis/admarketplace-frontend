290740ff83bf906af28d7f4b6b90f97fb22b9175
r742davis <richard.trow.davis@gmail.com>
2024-09-02 15:34:38 -0400

    Next config + Bundle Analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === 'true'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	devIndicators: {
+		buildActivityPosition: 'bottom-left'
+	},
+	distDir: 'build',
+	experimental: {
+		cssChunking: 'loose'
+	},
+	productionBrowserSourceMaps: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack: (config) => {
+		return config;
+	}
+};
+
+export default bundleAnalyzer(nextConfig);
