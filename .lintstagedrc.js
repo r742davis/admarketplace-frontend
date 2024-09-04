3c1fa5b109c93ee4a64e1d2cca9102ccaebb0781
r742davis <richard.trow.davis@gmail.com>
2024-09-03 22:10:53 -0400

    Chore: ESLint + Prettier config integrated into Husky

--- .lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-import { relative } from 'path';
-
-const buildEslintCommand = (filenames) => {
-	return `next lint --fix --file ${filenames.map((f) => relative(process.cwd(), f)).join(' --file ')}`;
-};
-
-export default {
-	'*.{js,jsx,ts,tsx}': [buildEslintCommand]
-};
