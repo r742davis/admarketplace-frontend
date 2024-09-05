98c2c3fad13de7137a4538a7edba2e4c88392b8c
r742davis <richard.trow.davis@gmail.com>
2024-09-05 15:23:54 -0400

    Task: Select Component

--- src/app/_components/Select/Select.tsx
+++ src/app/_components/Select/Select.tsx
@@ -1,3 +1,39 @@
-export default function Select() {
-	return <>Select</>;
+"use client";
+
+import { ChangeEvent } from "react";
+import styles from "./Select.module.scss";
+
+type Option = {
+	id: number;
+	value: string;
+	label?: string;
+};
+
+type SelectProps = {
+	options: Array<Option>;
+	value: string;
+	onChange: (event: ChangeEvent<HTMLSelectElement>, args: Array<unknown>) => void;
+	label?: string;
+};
+
+export default function Select({ options = [], value = "", onChange, label }: SelectProps) {
+	const handleChange = (event: ChangeEvent<HTMLSelectElement>, ...args: Array<unknown>) => {
+		onChange(event, args);
+	};
+
+	return (
+		<div className={styles["container"]}>
+			{label && <label htmlFor='select'>{label}</label>}
+			<select id='select' className={styles["select"]} value={value} onChange={handleChange}>
+				<option value='' disabled>
+					Please Select a Post
+				</option>
+				{options.map(option => (
+					<option key={option.value} value={option.value}>
+						{option?.label ?? option.value}
+					</option>
+				))}
+			</select>
+		</div>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
-import { getQueryClient } from "@/utils";
+"use client";
 
-export default function App() {
+import { ChangeEvent, useState } from "react";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Select } from "@/components";
+import { getQueryClient } from "@/lib";
+
+type Post = {
+	id: number;
+	title: string;
+	body: string;
+	comments?: [];
+};
+
+export default function Home() {
+	const [selectedValue, setSelectedValue] = useState("");
 	const queryClient = getQueryClient();
 
-	return <HydrationBoundary state={dehydrate(queryClient)}>Home</HydrationBoundary>;
+	const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		setSelectedValue(event.target.value);
+	};
+
+	const posts: Array<Post> = [
+		{ id: 1, title: "BLARGHHHHHH", body: "<Body Placeholder>" },
+		{ id: 2, title: "Post Title 2", body: "<Body Placeholder>" },
+		{ id: 3, title: "Post Title 3", body: "<Body Placeholder>" },
+		{ id: 4, title: "Post Title 4", body: "<Body Placeholder>" },
+	];
+
+	const mappedOptions = posts.map(p => ({ id: p.id, value: p.title }));
+
+	return (
+		<HydrationBoundary state={dehydrate(queryClient)}>
+			<Select
+				options={mappedOptions}
+				value={selectedValue}
+				onChange={handleChange}
+				label={"Please Select a Post"}
+			/>
+		</HydrationBoundary>
+	);
 }
