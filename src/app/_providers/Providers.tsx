c3dffc96406c0cde2abae1db70a59b4faf3fe9cf
r742davis <richard.trow.davis@gmail.com>
2024-09-07 12:38:01 -0400

    Chore: import order fix ESlint

--- src/app/_providers/Providers.tsx
+++ src/app/_providers/Providers.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
