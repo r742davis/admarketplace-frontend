f1e97e2c9a21795c82c561082526ae13428b4ba5
r742davis <richard.trow.davis@gmail.com>
2024-09-10 09:42:51 -0400

    Task: Global styles cleanup + CSS variables

--- src/app/posts/[postId]/error.tsx
+++ src/app/posts/[postId]/error.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import styles from "../../page.module.scss";
 
 export default function Error({ error }: { error: Error }) {
 	return (
-		<div>
+		<div className={styles["not-found"]}>
+			<h1>Uh oh! It looks like this post does not exist!</h1>
 			<h3>Invalid postId: {error.message}</h3>
 			<Link href='/'>Click here for the Home page</Link>
 		</div>
