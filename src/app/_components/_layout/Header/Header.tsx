fb3da841f9d00d1bfc7d51543c4cacaf3b054eac
r742davis <richard.trow.davis@gmail.com>
2024-09-11 11:45:34 -0400

    Task: Add link to home logo, fix p size

--- src/app/_components/_layout/Header/Header.tsx
+++ src/app/_components/_layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useRef, useEffect } from "react";
+import Link from "next/link";
 import cn from "classnames";
 import { NavigationMenu } from "@/components";
 import { SCROLL_DEBOUNCE_DELAY } from "@/lib";
@@ -36,7 +37,9 @@ export default function Header() {
 			className={cn(styles["header"], {
 				[styles["is-scrolled"]]: isScrolled,
 			})}>
-			<h1>postr</h1>
+			<Link href='/'>
+				<h1>postr</h1>
+			</Link>
 			<NavigationMenu />
 		</header>
 	);
