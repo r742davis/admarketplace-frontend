2493a3870e525c90058b8e0a616d0d4d3334e682
r742davis <richard.trow.davis@gmail.com>
2024-09-11 11:38:16 -0400

    Task: Style cleanup

--- src/app/_components/Comment/Comment.tsx
+++ src/app/_components/Comment/Comment.tsx
@@ -6,8 +6,8 @@ import { Comment as TComment } from "@/types";
 export default function Comment({ name = "", body = "" }: Partial<TComment>) {
 	return (
 		<li className={styles["container"]}>
-			<Avatar.Root className={styles.Root}>
-				<Avatar.Fallback className={styles.Fallback}>{name[0].toUpperCase()}</Avatar.Fallback>
+			<Avatar.Root className={styles["root"]}>
+				<Avatar.Fallback className={styles["fallback"]}>{name[0].toUpperCase()}</Avatar.Fallback>
 			</Avatar.Root>
 			<div className={styles["content"]}>
 				<strong>{capitalize(name)}</strong>
